12ed8319ef2d29bc251087c0c2ebf39676f35b0e
Ben <benbperry1@gmail.com>
2021-12-15 10:26:54 -0800

    commit tree f5f5f85fe3d90accdb91967eb062897b43102c5f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,43 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(num)
+        @happiness = num
+        if @happiness > 10
+                @happiness=10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+    end
+    
+    def hygiene=(num)
+        @hygiene = num
+        if @hygiene > 10
+                @hygiene=10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+    end
+
+end
+
+
+p1=Person.new("Ben")
+binding.pry
